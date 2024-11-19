ae59974cb393ab1d8d7c2cd02bb76946e4ad704e
RodrigoFDCG <r.fernandezgar.2023@alumnos.urjc.es>
2024-11-19 16:40:11 +0100

    Add files via upload

--- BoardService_R.js
+++ BoardService_R.js
@@ -6,38 +6,81 @@ import * as boardService from './boardService.js';
 
 const UPLOADS_FOLDER = 'uploads';
 const DEMO_FOLDER = 'demo';
+
+//Copy demo images to upload folder
 fs.cp(DEMO_FOLDER+'/image3.jpg', UPLOADS_FOLDER+'/image3.jpg');
 fs.cp(DEMO_FOLDER+'/image4.jpg', UPLOADS_FOLDER+'/image4.jpg');
 
-const comments = new Map();
-let commID=0;
+boardService.addCuadro({ 
+    nombre: 'Victorman', 
+    autor: 'JoanPoig', 
+    corriente: 'Barroco',
+    descripcion: 'No es el caballo Rumano', 
+    imagen: 'image3.jpg' 
+});
 
-const cuadros = new Map();
-let numCuadro=0;
-export function addComment(comment) {
-    let commID = commID++;
-    comments.set(commID, comment);
-}
-export function deleteComment(id) {
-    comments.delete(id);
-}
-export function editComment(comment, id){
-    comments.delete(id);
-    comments.set(id, comments)
-}
+boardService.addCuadro({ 
+    nombre: 'ElFonsi', 
+    autor: 'La Irene', 
+    corriente: 'Barroco',
+    descripcion: 'No es el caballo Rumano', 
+    imagen: 'image4.jpg' 
+});
 
-export function addCuadro(cuadro) {
-    let numCuadro = numCuadro++;
-    comments.set(numCuadro, cuadro);
-}
-export function deleteCuadro(id) {
-    cuadro.delete(id);
-}
-export function editCuadro(cuadro, id){
-    cuadro.delete(id);
-    cuadro.set(id, cuadro);
-}
+boardService.addCuadro({ 
+    nombre: 'Desconocido', 
+    autor: 'Alguien', 
+    corriente: 'No se sabe',
+    descripcion: 'alguien lo ha visto?', 
+    imagen: '' 
+});
 
-export function getCuadro(id){
-    return cuadros.get(id);
-}
+const router = express.Router();
+const upload = multer({ dest: UPLOADS_FOLDER })
+
+router.get('/', (req, res) => {
+
+    res.render('index', {
+        cuadros: boardService.getCuadros()
+    });
+});
+router.post('/cuadro/crear', upload.single('image'), (req, res) => {
+
+    let { nombre, autor, corriente, descripcion } = req.body;
+
+    let imageFilename = req.file.filename;
+
+    boardService.addCuadro({ nombre, autor, corriente, descripcion, imageFilename });
+
+    res.render('Cuadro guardado');
+});
+
+router.get('/cuadro/:id', (req, res) => {
+
+    let cuadro = boardService.getCuadro(req.params.id);
+
+    res.render('show_cuadro', { cuadro });
+});
+
+router.get('/cuadro/:id/delete', (req, res) => {
+
+    let cuadro = boardService.deleteCuadro(req.params.id);
+
+    if (cuadro) {
+        //Delete image. 
+        //It should be improved processing possible errors
+        fs.unlink(UPLOADS_FOLDER +'/' + post.imageFilename);
+    }
+
+    res.render('El cuadro se ha borrado');
+});
+
+router.get('/cuadro/:id/image', (req, res) => {
+
+    let cuadro = boardService.getCuadro(req.params.id);
+
+    res.download(UPLOADS_FOLDER + '/' + cuadro.imageFilename);
+
+});
+
+export default router;
